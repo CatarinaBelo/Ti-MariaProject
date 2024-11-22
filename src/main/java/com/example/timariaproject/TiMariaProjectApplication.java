02e57a87ee8591becf5c149e64f8555df5bf710e
CatarinaBelo <cabos1@iscte-iul.pt>
2024-11-22 17:41:26 +0000

    Initial commit

---
+++ src/main/java/com/example/timariaproject/TiMariaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.timariaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiMariaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiMariaProjectApplication.class, args);
+	}
+
+}
