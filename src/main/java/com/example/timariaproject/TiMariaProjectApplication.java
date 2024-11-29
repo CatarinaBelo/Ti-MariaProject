1993fc6c3b651591a4a10dfc727893e1d6049539
CatarinaBelo <cabos1@iscte-iul.pt>
2024-11-29 18:41:37 +0000

    Database já associada no projeto

--- src/main/java/com/example/timariaproject/TiMariaProjectApplication.java
+++ src/main/java/com/example/timariaproject/TiMariaProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TiMariaProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TiMariaProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TiMariaProjectApplication.class, args);
+    }
 
 }
